ebb98d031a4608d9952f56af020c9f80a36b3191
ajsehs0220-픽셀 <ajsehs0220@gmail.com>
2026-07-03 17:35:46 +0900

    metaFixel code 심기 완

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import Script from "next/script";
 import { mungyeongGamhong } from "@/lib/fonts";
 import "./globals.css";
 
-// GA4 측정 ID
 const GA_ID = "G-EXXRWBVXCW";
 
 export const metadata: Metadata = {
@@ -26,7 +25,6 @@ export default function RootLayout({
     <html lang="ko" className={mungyeongGamhong.variable}>
       <body>
         {children}
-
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
           strategy="afterInteractive"
@@ -39,9 +37,22 @@ export default function RootLayout({
             gtag('config', '${GA_ID}');
           `}
         </Script>
-
         <Script id="clarity" strategy="afterInteractive">
           {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","xg6qrs1cvm");`}
+        </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '4582857182037515');
+            fbq('track', 'PageView');
+          `}
         </Script>
       </body>
     </html>
