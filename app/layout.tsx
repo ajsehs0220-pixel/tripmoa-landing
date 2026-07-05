9ad12d7049517aa84f912d6048096b7569fbbe5b
ajsehs0220-픽셀 <ajsehs0220@gmail.com>
2026-07-06 00:13:53 +0900

    wait() 폴링이 먼저 실행해결2

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,16 +39,16 @@ export default function RootLayout({
             gtag('config', '${GA_ID}');
           `}
         </Script>
-        <Script
-          id="clarity"
-          src="https://www.clarity.ms/tag/xg6qrs1cvm"
-          strategy="afterInteractive"
-          onLoad={() => {
-            if (typeof window.clarity === 'function') {
-              window.clarity('consent');
-            }
-          }}
-        />
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window,document,"clarity","script","xg6qrs1cvm");
+            window.clarity("consent");
+          `}
+        </Script>
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
